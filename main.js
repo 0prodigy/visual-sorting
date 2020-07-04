25a7eed8c1988b80f4ec449f76eb4d0fe6a8cdd6
Akash <pathakvikash9211@gmail.com>
2020-07-04 11:19:15 +0530

    Added bubble sort function

--- main.js
+++ main.js
@@ -30,3 +30,28 @@ function renderArr(arr) {
   }
   // console.log(arr);
 }
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const swapIndex = (arr, i, j) => {
+  let col = document.querySelectorAll(".row div");
+  col[i].style.border = "5px solid red";
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+  return arr;
+};
+
+async function bubbelSort(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 1; j <= i; j++) {
+      if (arr[j - 1] > arr[j]) {
+        swapIndex(arr, j, j - 1);
+        await drawArr(arr);
+        await sleep(time);
+      }
+    }
+  }
+}
